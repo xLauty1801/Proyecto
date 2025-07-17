221c68ef36084c7c79ca65c68e042701851bad54
Lautaro <lautarovini2@gmail.com>
2025-07-17 19:10:38 -0300

    Por ultimo, agregue un carrito de compras

---
+++ Pagina/js/main.js
@@ -0,0 +1,38 @@
+const carrito = [];
+
+function actualizarCarrito() {
+    const lista = document.getElementById('lista-carrito');
+    const totalSpan = document.getElementById('total');
+    lista.innerHTML = ''; // Por las dudas me aseguro de dejarlo vacio
+    let total = 0;
+
+    carrito.forEach(item => {
+      const li = document.createElement('li');
+      li.textContent = `${item.nombre} - $${item.precio} USD x ${item.cantidad}`;
+      lista.appendChild(li);
+      total += item.precio * item.cantidad;
+    });
+
+    totalSpan.textContent = total.toFixed(2);
+}
+
+function agregarAlCarrito(nombre, precio) {
+    const productoExistente = carrito.find(item => item.nombre === nombre);  
+    if (productoExistente) {
+      productoExistente.cantidad++;
+      // si el producto ya esta en el carrito, le sumo uno a la cantidad
+    } else {
+      carrito.push({ nombre, precio: Number(precio), cantidad: 1 });
+    }
+    actualizarCarrito();
+  }
+
+document.querySelectorAll('.agregar-carrito').forEach(boton => {
+    boton.addEventListener('click', () => {
+      const nombre = boton.getAttribute('data-nombre');
+      // console.log(nombre)
+      const precio = boton.getAttribute('data-precio');
+      // console.log(precio)
+      agregarAlCarrito(nombre, precio);
+    });
+  });
